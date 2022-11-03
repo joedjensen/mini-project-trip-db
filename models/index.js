86936680641e223dde911510f8dfb17b6d92aefd
josephjensen <joe@cryptonumerics.com>
2022-11-03 12:46:50 -0700

    add unique:false to belongstomany

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const Location = require('./Location');
 const Traveller = require('./Traveller');
 const Trip = require('./Trip');
 
-Location.belongsToMany(Traveller, { through: Trip, foreignKey: 'location_id'} )
-Traveller.belongsToMany(Location, { through: Trip, foreignKey: 'traveller_id'} )
+Location.belongsToMany(Traveller, { through: Trip, foreignKey: 'location_id', unique:false } )
+Traveller.belongsToMany(Location, { through: Trip, foreignKey: 'traveller_id', unique: false} )
 
 module.exports = { Location, Traveller, Trip };
